69809523f90fd3f3469436986ea276675fe401f3
Poojaaaa <Pooja.gajera11@gmail.com>
2019-02-20 22:46:56 -0800

    adding styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
   render() {
@@ -13,19 +15,21 @@ export default class Navbar extends Component {
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
-          <ul className="navbar-nav align-items-center">
-            <li className="nav-item ml-5">
-              <Link to="/" className="nav-link">
-                Products
-              </Link>
-            </li>
-          </ul>
-          <Link to="/cart" className="ml-auto">
-            <button>
-              <i className="fas fa-cart-plus"></i>
-              My Cart
-            </button>
-          </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            My Cart
+          </ButtonContainer>
+        </Link>
       </nav>
     );
   }
