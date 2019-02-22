c5370e23c0c5b98280a91e5ba51ddb5e34a7d5f1
Poojaaaa <Pooja.gajera11@gmail.com>
2019-02-22 00:06:51 -0800

    Adding details page

--- src/context.js
+++ src/context.js
@@ -24,12 +24,20 @@ export default class ProductProvider extends Component {
     });
   };
 
-  handleDetail = () => {
-    console.log("hello from detail!");
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
   };
 
-  addToCart = () => {
-    console.log("hello from add to cart!!");
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
+  };
+
+  addToCart = id => {
+    let tempProducts = []
   };
 
   render() {
